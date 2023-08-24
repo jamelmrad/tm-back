22107f45ee482ba76dadb6ff4773bb04e62b430a
Fiascooo <jameledinemrad@samrise.cloud>
2023-08-24 16:12:39 +0100

    1.4

--- api-gateway/src/main/java/com/telcotek/apigateway/repository/VisitorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.telcotek.apigateway.repository;
-
-import com.telcotek.apigateway.model.Visitor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
-}
